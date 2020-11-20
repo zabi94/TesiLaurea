b2f25ddb770b0be916d61c147eebf178227e3733
Simone Zanin <zabi94@users.noreply.github.com>
2020-11-20 23:20:30 +0100

    Infopage minor changes

--- lib/pages/InfoPage.dart
+++ lib/pages/InfoPage.dart
@@ -11,47 +11,50 @@ class InfoPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: InkWell(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(Icons.label, size: 170, color: Colors.orange,),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.asset('assets/images/uniud.png', height: 130, width: 130,),
-                          ),
-                        ],
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: InkWell(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.label, size: 170, color: Colors.orange,),//TODO cambiare con l'icona dell'app
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset('assets/images/uniud.png', height: 130, width: 130,),
+                            ),
+                          ],
+                        ),
+                        onLongPress: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Dedica"),
+                                content: Text("A chi mi ha dato la forza di andare avanti."),
+                              );
+                            }
+                          );
+                        },
                       ),
-                      onLongPress: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: Text("Dedica"),
-                              content: Text("A chi mi ha dato la forza di andare avanti."),
-                            );
-                          }
-                        );
-                      },
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text("Image Tagger", textScaleFactor: 2,),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text("Image Tagger", textScaleFactor: 2,),
+                    ),
+                  ],
+                ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 68.0),
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
