f7334636c896882ac637ab487095b5db57d76b87
Simone Zanin <zabi94@users.noreply.github.com>
2021-01-10 18:24:34 +0100

    Cloud icon in summary starts upload now

--- lib/Widgets/PictureSummaryCard.dart
+++ lib/Widgets/PictureSummaryCard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tesi_simone_zanin_140833/PersistentData.dart';
+import 'package:tesi_simone_zanin_140833/upload_service/UploadManager.dart';
 
 class PictureSummaryCard extends StatelessWidget {
 
@@ -64,7 +65,7 @@ class PictureSummaryCard extends StatelessWidget {
                         child: FutureBuilder(
                           future: context.watch<DatabaseInterface>().getByPath(record.getFilePath()),
                           builder: (ctx, snap) {
-                            Icon ico = Icon(Icons.cloud, color: Colors.grey, key: UniqueKey(),);
+                            Widget ico = Icon(Icons.cloud, color: Colors.grey, key: UniqueKey(),);
                             if (snap.hasData) {
                               if (snap.data.uploadedTo != null && snap.data.uploadedTo.isNotEmpty) {
                                 ico = Icon(Icons.cloud_done, color: Colors.green, key: UniqueKey(),);
@@ -72,12 +73,41 @@ class PictureSummaryCard extends StatelessWidget {
                                 ico = Icon(Icons.cloud_upload, color: Colors.orangeAccent, key: UniqueKey(),);
                               }
                             }
-                            return Hero(
-                              tag: "status-icon_${record.getFilePath()}",
-                              child: AnimatedSwitcher(
-                                child: ico,
-                                duration: Duration(milliseconds: 200),
+                            return InkWell(
+                              child: Hero(
+                                tag: "status-icon_${record.getFilePath()}",
+                                child: AnimatedSwitcher(
+                                  child: ico,
+                                  duration: Duration(milliseconds: 200),
+                                ),
                               ),
+                              onTap: () {
+                                if (snap.hasData && (snap.data.uploadedTo == null || snap.data.uploadedTo.isEmpty)) {
+                                  Scaffold.of(context).showSnackBar(SnackBar(
+                                    content: Text("Caricamento"),
+                                    duration: Duration(seconds: 1),
+                                  ));
+                                  UploadManager.uploadSingleJob(snap.data)
+                                    .catchError((err) {
+                                      Scaffold.of(context).showSnackBar(SnackBar(
+                                        content: Text("Errore durante il caricamento: $err"),
+                                        duration: Duration(seconds: 5),
+                                      ));
+                                    }).then((value) {
+                                      if (value ~/ 100 != 2) {
+                                        Scaffold.of(context).showSnackBar(SnackBar(
+                                          content: Text("Caricamento fallito: $value"),
+                                          duration: Duration(seconds: 5),
+                                        ));
+                                      } else {
+                                        Scaffold.of(context).showSnackBar(SnackBar(
+                                          content: Text("Caricamento completato: $value"),
+                                          duration: Duration(seconds: 5),
+                                        ));
+                                      }
+                                    });
+                                }
+                              },
                             );
                           },
                         ),
